0334ae617f488fb9c0068563b25a8048b6f4a941
Michael <kilbyjmichael@gmail.com>
2015-04-07 23:21:58 -0500

    Delete py_bot.py
    Deleted the bad version

--- PyBot.py
+++ PyBot.py
@@ -12,8 +12,14 @@ irc = IRC() # constructor!
 irc.connect(HOST, PORT, CHAN, NICK)
 
 while True:
-    data = irc.get_data()
-    print data
+    try:
+        data = irc.get_data()
+        print data
 
-    if "PRIVMSG" in data and CHAN in data and "!test" in data:
-        irc.send_msg(CHAN, "You tested me!")
+        if "PRIVMSG" in data and CHAN in data and "!test" in data:
+            irc.send_msg(CHAN, "You tested me!")
+            
+    finally:
+    # cleanup!
+    irc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    irc.shutdown(socket.SHUT_RDWR)
